cdc8d2d96b12aa567f430bd04355807e54317417
Mathias Immerkær <mim@cvation.com>
2020-04-05 11:02:17 +0200

    Initial

---
+++ SignalRTypeScriptGenerator/SignalRTypeScriptGeneratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Reinforced.Typings.Ast.TypeNames;
+using Reinforced.Typings.Fluent;
+
+namespace SignalRTypeScriptGenerator
+{
+    public static class SignalRTypeScriptGeneratorExtensions
+    {
+        public static void GenerateSignalRTypeScript(this ConfigurationBuilder builder)
+        {
+            builder.AddImport("{ Injectable }", "@angular/core");
+            builder.AddImport("{ HubConnection }", "@microsoft/signalr");
+            builder.AddImport("* as signalR", "@microsoft/signalr");
+            builder.AddImport("{ SignalDispatcher, SimpleEventDispatcher }", "strongly-typed-events");
+            builder.AddImport("{ HubConnectionProvider }", "./hubconnectionprovider.service");
+
+            builder.Substitute(typeof(Task), new RtSimpleTypeName("Promise<void>"));
+            builder.Substitute(typeof(Task<>), new RtSimpleTypeName("Promise"));
+        }
+    }
+}
