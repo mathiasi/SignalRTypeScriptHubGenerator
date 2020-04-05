bb04e28476a67f03915635128533f5c80e7f00b9
Mathias Immerkær <mim@cvation.com>
2020-04-05 12:56:36 +0200

    Remove import

--- SignalRTypeScriptHubGenerator/SignalRTypeScriptHubGeneratorExtensions.cs
+++ SignalRTypeScriptHubGenerator/SignalRTypeScriptHubGeneratorExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
 using Reinforced.Typings;
 using Reinforced.Typings.Ast;
 using Reinforced.Typings.Ast.TypeNames;
